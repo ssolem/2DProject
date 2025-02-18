368b05e1a9d9b92c33bd0e415b8c9782ea279104
ssolem <tjrals961@gmail.com>
2025-02-18 17:25:42 +0900

    [fix] firstscene player
    입력 없을 시 정지 하게끔 수정

--- Assets/Scripts/FirstScene/Player.cs
+++ Assets/Scripts/FirstScene/Player.cs
@@ -57,6 +57,8 @@ public class Player : MonoBehaviour
 
     private void Move(float direction)
     {
+        if (direction == 0) 
+            Stop(direction);
         direction = direction * playerSpeed;
         transform.Translate(Vector3.right * direction);
 
@@ -65,11 +67,16 @@ public class Player : MonoBehaviour
 
     private void Stop(float direction)
     {
-
+        transform.position = new Vector3(transform.position.x, transform.position.y);
     }
 
     private void Look(float direction)
     {
+        if (direction == 0)
+        {
+            spriteRenderer.flipX = spriteRenderer.flipX;
+            return;
+        }
         bool isRIght = direction > 0;
 
         spriteRenderer.flipX = isRIght ? false : true;
@@ -87,6 +94,13 @@ public class Player : MonoBehaviour
     void OnMove(InputValue inputValue)
     {
         moveDirection = inputValue.Get<float>();
+        Debug.Log(moveDirection);
+        if(moveDirection != 1 && moveDirection != -1)
+        {
+            moveDirection = 0;
+            Debug.Log(moveDirection);
+
+        }
     }
 
     void OnJump(InputValue inputValue)
